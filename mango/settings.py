99427f303428c98a936dce9b5092809a1680f08c
artificodes <59394290+artificodes@users.noreply.github.com>
2022-09-20 13:42:31 +0100

    Update

--- mango/settings.py
+++ mango/settings.py
@@ -208,43 +208,43 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-AWS_ACCESS_KEY_ID = env.str('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = env.str('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = env.str('AWS_STORAGE_BUCKET_NAME')
-AWS_DEFAULT_ACL = None
-AWS_LOCATION = 'static'
-AWS_MEDIA_LOCATION = 'media'
-STATIC_URL = 'https://%s.s3.amazonaws.com/%s/' % (AWS_STORAGE_BUCKET_NAME, AWS_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-DEFAULT_FILE_STORAGE = 'mango.storages.MediaStorage'
+# AWS_ACCESS_KEY_ID = env.str('AWS_ACCESS_KEY_ID')
+# AWS_SECRET_ACCESS_KEY = env.str('AWS_SECRET_ACCESS_KEY')
+# AWS_STORAGE_BUCKET_NAME = env.str('AWS_STORAGE_BUCKET_NAME')
+# AWS_DEFAULT_ACL = None
+# AWS_LOCATION = 'static'
+# AWS_MEDIA_LOCATION = 'media'
+# STATIC_URL = 'https://%s.s3.amazonaws.com/%s/' % (AWS_STORAGE_BUCKET_NAME, AWS_LOCATION)
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# DEFAULT_FILE_STORAGE = 'mango.storages.MediaStorage'
 
-# USE_SPACES = os.getenv('USE_SPACES') == 'TRUE'
+USE_SPACES = os.getenv('USE_SPACES') == 'TRUE'
 
-# if USE_SPACES:
-#     # settings
-#     AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-#     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-#     AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
-#     AWS_DEFAULT_ACL = 'public-read'
-#     AWS_S3_ENDPOINT_URL = 'https://nyc3.digitaloceanspaces.com'
-#     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
-#     # static settings
-#     AWS_LOCATION = 'static'
-#     STATIC_URL = f'https://{AWS_S3_ENDPOINT_URL}/{AWS_LOCATION}/'
-#     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-#     # public media settings
-#     PUBLIC_MEDIA_LOCATION = 'media'
-#     MEDIA_URL = f'https://{AWS_S3_ENDPOINT_URL}/{PUBLIC_MEDIA_LOCATION}/'
-#     DEFAULT_FILE_STORAGE = 'mango.storage_backends.PublicMediaStorage'
+if USE_SPACES:
+    # settings
+    AWS_ACCESS_KEY_ID = 'DO00G8JCCQVQPZK7WZDB'
+    AWS_SECRET_ACCESS_KEY = 'WR0J3qXy1QenrujIFx1qgEJO9WtlqMrtVLg4ac+2zAI'
+    AWS_STORAGE_BUCKET_NAME = 'mgoapi_victor'
+    AWS_DEFAULT_ACL = 'public-read'
+    AWS_S3_ENDPOINT_URL = 'https://nyc3.digitaloceanspaces.com'
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    # static settings
+    AWS_LOCATION = 'static'
+    STATIC_URL = f'https://{AWS_S3_ENDPOINT_URL}/{AWS_LOCATION}/'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    # public media settings
+    PUBLIC_MEDIA_LOCATION = 'media'
+    MEDIA_URL = f'https://{AWS_S3_ENDPOINT_URL}/{PUBLIC_MEDIA_LOCATION}/'
+    DEFAULT_FILE_STORAGE = 'mango.storage_backends.PublicMediaStorage'
 
-# # private media settings
-#     PRIVATE_MEDIA_LOCATION = 'private'
-#     PRIVATE_FILE_STORAGE = 'mango.storage_backends.PrivateMediaStorage'
-# else:
-#     STATIC_URL = '/static/'
-#     STATIC_ROOT = BASE_DIR / 'staticfiles'
-#     MEDIA_URL = '/media/'
-#     MEDIA_ROOT = BASE_DIR / 'mediafiles'
+# private media settings
+    PRIVATE_MEDIA_LOCATION = 'private'
+    PRIVATE_FILE_STORAGE = 'mango.storage_backends.PrivateMediaStorage'
+else:
+    STATIC_URL = '/static/'
+    STATIC_ROOT = BASE_DIR / 'staticfiles'
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = BASE_DIR / 'mediafiles'
 
-# STATICFILES_DIRS = (BASE_DIR / 'static',)
+STATICFILES_DIRS = (BASE_DIR / 'static',)
 
