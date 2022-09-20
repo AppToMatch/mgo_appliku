55592b4a93f142f03749f3249c8d6f0082ab2dbd
artificodes <59394290+artificodes@users.noreply.github.com>
2022-09-20 11:18:56 +0100

    Updated models

--- app/models.py
+++ app/models.py
@@ -21,7 +21,6 @@ from django.shortcuts import get_object_or_404
 from imagekit.models import ImageSpecField # < here
 from pilkit.processors import ResizeToFill
 from random import random
-from tinymce.models import HTMLField
 import os
 import base64
 from django.conf import settings
